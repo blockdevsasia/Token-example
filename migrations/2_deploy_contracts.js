145c92003c7b85fc8f5e9d105f6f373c7fcef410
coostendorp <coostendorp@vinca.ph>
2018-09-23 15:26:49 +0800

    Added Vue.js to the token example. NOTES: - No functionality implemented yet (but that should be easy now) - Borrowed some Vue approaches from https://github.com/kyriediculous/dapp-tutorial - Added a special script in 2_deploy_contracts that creates the constants js contract file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,26 @@
+var fs = require('fs');
+
 var BitcoinPH = artifacts.require("./BitcoinPH.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(BitcoinPH, 21000000);
+// Run this function async so we can use the cleaner "await" syntax within
+module.exports = async function(deployer) {
+  // Deploy our token
+  await deployer.deploy(BitcoinPH, 21000000)
+
+  // Get a handle of the deployed instance so we can read stuff from it
+  instance = await BitcoinPH.deployed()
+
+  // Assemble the constants javascript file from the instance object
+  fileContent = "const address = '" + instance.address + "';\n"
+  fileContent += "const ABI = " + JSON.stringify(instance.abi) + ";\n"
+  fileContent += "export {address, ABI};"
+
+  // Build the filepath for the constants file
+  let filepath = "./src/util/constants/" + instance.constructor.contractName + "Contract.js"
+
+  // Write the file
+  fs.writeFile(filepath, fileContent, (err) => {
+    if (err) throw err;
+  });
+
 };
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <token-dapp></token-dapp>
+    <hr/>
+    <hello-metamask></hello-metamask>
+  </div>
+
+
+</template>
+
+<script>
+import TokenDapp from './components/token-dapp'
+import HelloMetamask from './components/hello-metamask'
+
+export default {
+  name: 'app',
+  components: {
+    HelloMetamask,
+    TokenDapp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
